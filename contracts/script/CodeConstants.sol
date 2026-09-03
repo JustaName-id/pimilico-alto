ab8788a2dd1a70e4847772ecd0389d4666f41fa1
Ghadi Mhawej <ghadimhawej@outlook.com>
2026-09-03 11:26:57 +0300

    feat: add support for Hyvechain

--- contracts/script/CodeConstants.sol
+++ contracts/script/CodeConstants.sol
@@ -48,6 +48,8 @@ abstract contract CodeConstants {
 
     uint256 public constant SONEIUM_CHAIN_ID = 1868;
 
+    uint256 public constant HYVECHAIN_CHAIN_ID = 7847;
+
     // Address of the v0.8 EntryPoint contract
     address public constant ENTRYPOINT_ADDRESS = 0x4337084D9E255Ff0702461CF8895CE9E3b5Ff108;
 
