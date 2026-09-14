5930cae7d21da4e201f24953586e0649cd9b9680
Ghadi Mhawej <ghadimhawej@outlook.com>
2026-09-14 10:33:34 +0300

    feat: add support for Monad

--- contracts/script/CodeConstants.sol
+++ contracts/script/CodeConstants.sol
@@ -52,6 +52,8 @@ abstract contract CodeConstants {
 
     uint256 public constant UNICHAIN_CHAIN_ID = 130;
 
+    uint256 public constant MONAD_CHAIN_ID = 143;
+
     // Address of the v0.8 EntryPoint contract
     address public constant ENTRYPOINT_ADDRESS = 0x4337084D9E255Ff0702461CF8895CE9E3b5Ff108;
 
